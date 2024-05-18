2beaf487b6eeb4e5348904e83511540d00512657
feramig <feram_caa@hotmail.com>
2024-05-18 05:30:50 -0400

    modify book endpoints

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,32 +12,43 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  res.send(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  let filtered_authors = {};
+  for(let book in books){
+    if(books[book].author===author){
+        filtered_authors = books[book];
+    }
+  }
+  res.send(filtered_authors);
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title;
+    let filtered_titles = {};
+    for(let book in books){
+        if(books[book].title===title){
+            filtered_titles = books[book];
+        }
+    }
+    res.send(filtered_titles);
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let book = books[req.params.isbn];
+  res.send(book.reviews);
 });
 
 module.exports.general = public_users;
